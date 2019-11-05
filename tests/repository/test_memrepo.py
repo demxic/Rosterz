e1e939445e7de5d83452ff49c678ae3ad5dd1033
Xicotencatl_PC <demxic@gmail.com>
2019-11-05 14:56:06 -0600

    feat: Add the Mem repository class
    with its corresponding pytest.
    
    This easy class just helps with creating a simulated repository

---
+++ tests/repository/test_memrepo.py
@@ -0,0 +1,21 @@
+import pytest
+
+from tripper.domain.elements import Station
+from tripper.repository import memrepo
+
+
+@pytest.fixture
+def airport_dicts():
+    return [
+        {'code': 'MEX', 'continent': 'America', 'tz_city': 'Mexico_City', 'viaticum': None},
+        {'code': 'GDL', 'continent': 'America', 'tz_city': 'Mexico_City', 'viaticum': None},
+        {'code': 'JFK', 'continent': 'America', 'tz_city': 'New_York', 'viaticum': None},
+        {'code': 'MAD', 'continent': 'Europe', 'tz_city': 'Madrid', 'viaticum': None}
+    ]
+
+
+def test_repository_list_without_parameters(airport_dicts):
+    repo = memrepo.MemRepo(airport_dicts)
+
+    airports = [Station.from_dict(a) for a in airport_dicts]
+    assert repo.list() == airports
---
+++ tripper/repository/memrepo.py
@@ -0,0 +1,10 @@
+from tripper.domain.elements import Station
+
+
+class MemRepo(object):
+
+    def __init__(self, data):
+        self.data = data
+
+    def list(self):
+        return [Station.from_dict(a) for a in self.data]
